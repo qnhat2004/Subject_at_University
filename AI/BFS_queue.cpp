0c4145c4eed18c55b17d85cbe8e7b4ad8fb6e921
Vũ Quang Nhật <126136518+qnhat2004@users.noreply.github.com>
2023-11-29 16:23:35 +0700

    Create BFS_queue.cpp

---
+++ AI/BFS_queue.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Giả sử cho danh sách cạnh 1-4, 2-3,... ta phải chuyển danh sách cạnh thành danh sách kề*/
+
+int main()
+{
+    int n, m; // Sổ đỉnh, số cạnh
+    cin >> n >> m;
+
+    vector<int> adj[n + 1]; // Adjacecy list: danh sách kề
+
+    for (int i = 0; i < m; ++i)
+    {
+        int u, v; cin >> u >> v; // Lần lươt nhập cạnh u-v
+        adj[u].push_back(v);
+        adj[v].push_back(u); // Nếu đồ thị có hướng tức là u->v, ta chỉ được thêm v vào danh sách kề của u chứ không có dòng này
+    }
+    
+    queue<int> q;
+    q.push(1);
+
+    bool visited[n + 1];
+    memset(visited, false, sizeof(visited));
+
+    while (q.size())
+    {
+        int curr = q.top();
+        cout << curr << " ";
+        q.pop();
+
+        for (int v : adj(curr)) // Duyệt các đỉnh kề với curr
+        {
+            if (!visited[v]) // Nếu v chưa được thăm
+            {
+                q.push(v);
+                visited[v] = true; // Đánh dấu là v đã được thăm
+            }
+        }
+    }   
+    
+    return 0;
+}
+1
