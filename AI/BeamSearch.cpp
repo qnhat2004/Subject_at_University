1259ed2f6f07dbc6fccf4e6c4b08eb83e4ca8e03
Vũ Quang Nhật <126136518+qnhat2004@users.noreply.github.com>
2023-12-04 00:33:50 +0700

    Update BeamSearch.cpp

--- AI/BeamSearch.cpp
+++ AI/BeamSearch.cpp
@@ -1,55 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-/* input1:
-3
-5 5 
-A E
-A B
-B C
-A D
-B D
-A E
-A 10
-B 20
-C 30
-D 40
-E 50
-*/
-
-/* input2:
-2
-8 11
-S A
-S F
-S A
-F B
-A B
-A E
-B E
-E C
-B C
-B D
-D G
-C G
-S 8
-F 4
-A 5
-B 4
-E 3
-D 1
-C 1
-G 0
-*/
-
-// Beam Search
-// Cho vào open mới sắp xếp lại theo hàm heuristic
 typedef pair<char, int> node;
 
 class compare
 {
     public:
-        bool operator()(node const &a, node const &b) // Call function (Hàm gọi), 
+        bool operator()(node const &a, node const &b)
         {
             return a.second > b.second;
         }
@@ -58,7 +15,7 @@ class compare
 string getPath(char start, char target, map<char, char> &parent)
 {
     string path = "";
-    for (char v = target; v != start; v = parent[v]) // Lần ngược lại các đỉnh cha của target để tìm đường đi đã tìm thấy
+    for (char v = target; v != start; v = parent[v])
         path =  v + " " + path;
     path = start + " " + path;
     return path;
@@ -66,7 +23,7 @@ string getPath(char start, char target, map<char, char> &parent)
 
 int main()
 {
-    int k; // Số đỉnh tối đa được đưa vào hàng đợi ưu tiên tại mỗi bước
+    int k;
     cout << "Enter k: "; cin >> k;
 
     int n, m;
@@ -80,11 +37,10 @@ int main()
     for (int i = 0; i < m; ++i)
     {
         char u, v; cin >> u >> v;
-        adj[u].push_back(v); // Directed Graph - Đồ thị có hường
-        // adj[v].push_back(u); // Undirected Graph - Đồ thị vô hướng
+        adj[u].push_back(v);
     }
 
-    map<char, int> heuristic; // hàm heauristic của đỉnh i
+    map<char, int> heuristic;
     for (int i = 0; i < n; ++i)
     {
         char v; int hv;
@@ -93,17 +49,17 @@ int main()
     }
 
     map<char, bool> visited;
-    visited[start] = true;
+    map<char, char> parent;
 
     priority_queue<node, vector<node>, compare> pq;
     pq.push(make_pair(start, heuristic[start]));
 
-    map<char, char> parent;
-
     while (pq.size())
     {
         char cur = pq.top().first; pq.pop();
-        // cout << cur << ' ' << heuristic[cur] << endl;
+
+        if (visited[cur]) continue;
+        visited[cur] = true;
 
         if (cur == target)
         {
@@ -112,22 +68,21 @@ int main()
             cout << path << endl;
             return 0;
         }
-        
+
+        vector<node> next_nodes;
         for (auto &next : adj[cur])
         {
-            if (visited.find(next) == visited.end())
+            if (!visited[next])
             {
-                // if (next == target)
-                // {
-                //     cout << "Found\n";
-                //     string path = getPath(start, target, parent);
-                //     cout << path << endl;                           
-                //     return 0;
-                // }
-                visited[next] = true;
-                pq.push({next, heuristic[next]});
+                next_nodes.push_back({next, heuristic[next]});
                 parent[next] = cur;
             }
+        }
+
+        sort(next_nodes.begin(), next_nodes.end(), compare());
+        for (int i = 0; i < min(k, (int)next_nodes.size()); ++i)
+        {
+            pq.push(next_nodes[i]);
         }
     }
     cout << "Not found";
