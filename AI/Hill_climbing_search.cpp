4e19efbffd09ceb09568786272b9b6d74fa87d2d
Vũ Quang Nhật <126136518+qnhat2004@users.noreply.github.com>
2023-12-04 00:24:03 +0700

    Create Hill_climbing_search.cpp

---
+++ AI/Hill_climbing_search.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>   
+using namespace std;
+
+// Hill-climbing algorithm - Thuật toán leo đồi
+
+typedef pair<char, int> node;
+
+class compare
+{
+    public:
+        bool operator () (const node &a, const node &b)
+        {
+            return a.second < b.second;
+        }
+};
+
+bool dfs(char from, char to, map<char, vector<char>> &adj, map<char, int> &h)
+{
+    map<char, bool> visited;
+    visited[from] = true;
+
+    stack<char> st;
+    st.push(from);
+
+    priority_queue<node, vector<node>, compare> pq;
+    pq.push({from, from});
+
+    while (st.size())
+    {
+        char cur = st.top(); st.pop();
+
+        if (cur == to) return true;
+
+        for (auto &i : adj[cur])
+        {
+            if (!visited[i])
+            {
+                pq.push({i, h[i]});
+            }
+            
+        }
+        while (pq.size())
+        {
+            char next = pq.top().first;
+            pq.pop();
+            visited[next] = true;
+            st.push(next);
+        }
+    }
+    
+    return false;
+}
+
+int main()
+{
+    int n, m;
+    cout << "Row, collum: "; cin >> n >> m;
+
+    cout << "Edges list:\n";
+    map<char, vector<char>> adj;
+
+    for (int i = 0; i < m; ++i)
+    {
+        char a, b; cin >> a >> b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }    
+
+    char from, to;
+    cout << "Start, end: "; cin >> from >> to;
+
+    cout << "Heuristic:\n";
+    map<char, int> h; // heuristic
+    for (auto &i : adj) cin >> h[i.first];
+        
+    cout << "Result: " << dfs(from, to, adj, h) << endl;
+}
