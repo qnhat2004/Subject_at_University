28f3b5d5be4d690d8d31298785f3215509fee15b
Vũ Quang Nhật <126136518+qnhat2004@users.noreply.github.com>
2023-12-05 09:40:07 +0700

    Create 5_12_2023_bai02.cpp

---
+++ OOP/5_12_2023_bai02.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Point
+{
+    double x, y;
+
+    // Cấu tử - Constructor
+    Point(double x, double y)
+    {
+        this->x = x;
+        this->y = y;
+    }
+
+    ~Point()
+    {
+        // Hàm hủy
+    }
+
+    friend istream &operator >> (istream &in, Point &p)
+    {
+        cout << "x, y = ";
+        in >> p.x >> p.y;
+        return in;
+    }
+
+    friend ostream &operator << (ostream &out, Point &p)
+    {
+        cout << "(" << p.x << ", " << p.y << ")" << endl;
+    }
+
+    friend double KhoangCach (const Point &a, const Point &b)
+    {
+        return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2));
+    }
+};
+
+class Dagiac
+{
+    vector<Point> a;
+    int Size;
+
+    public:
+        Dagiac(int Size)
+        {
+            this->Size = Size;
+            a.resize(Size);
+        }
+
+        ~Dagiac()
+        {
+            // Hàm hủy
+            a.clear();
+        }
+
+        friend bool KiemTra(const Dagiac &d)
+        {
+            if (d.Size != 3) return false;
+            double ab = KhoangCach(d.a[0], d.a[1]);
+            double bc = KhoangCach(d.a[1], d.a[2]);
+            double ca = KhoangCach(d.a[2], d.a[0]);
+            return (ab + bc > ca && ab + ca > bc &&  bc + ca > ab);
+        }
+
+        friend bool KiemTraCan(const Dagiac &d)
+        {
+            if (!KiemTra(d)) return false;
+            double ab = KhoangCach(d.a[0], d.a[1]);
+            double bc = KhoangCach(d.a[1], d.a[2]);
+            double ca = KhoangCach(d.a[2], d.a[0]);
+            return (ab == bc || bc == ca || ca == ab);
+        }
+};
+
+int main()
+{
+    int n;
+    cout << "Nhap so luong da giac: "; cin >> n;
+
+    vector<Dagiac> a(n);
+
+    for (int i = 0; i < n; ++i)
+    {
+        
+    }
+    return 0;
+}
