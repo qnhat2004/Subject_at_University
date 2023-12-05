e78e9763754d579980e6ef0afeecfdf785196390
Vũ Quang Nhật <126136518+qnhat2004@users.noreply.github.com>
2023-12-05 16:22:13 +0700

    Update Point.cpp

--- OOP/Point.cpp
+++ OOP/Point.cpp
@@ -5,33 +5,35 @@ class Point
 {
     double x, y;
 
-    // Cấu tử - Constructor
-    Point()
-    {
-       x = y = 0;
-    }
-
-    ~Point()
-    {
-        // Hàm hủy
-    }
-
-    friend istream &operator >> (istream &in, Point &p)
-    {
-        cout << "x, y = ";
-        in >> p.x >> p.y;
-        return in;
-    }
-
-    friend ostream &operator << (ostream &out, Point &p)
-    {
-        cout << "(" << p.x << ", " << p.y << ")" << endl;
-    }
-
-    friend double KhoangCach (const Point &a, const Point &b)
-    {
-        return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2));
-    }
+    public:
+        // Cấu tử - Constructor
+        Point()
+        {
+           x = y = 0;
+        }
+    
+        ~Point()
+        {
+            // Hàm hủy
+        }
+    
+        friend istream &operator >> (istream &in, Point &p)
+        {
+            cout << "x, y = ";
+            in >> p.x >> p.y;
+            return in;
+        }
+    
+        friend ostream &operator << (ostream &out, Point &p)
+        {
+            cout << "(" << p.x << ", " << p.y << ")";
+            return out;
+        }
+    
+        friend double KhoangCach (const Point &a, const Point &b)
+        {
+            return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2));
+        }
 };
 
 class Dagiac
@@ -52,6 +54,10 @@ class Dagiac
             a.clear();
         }
 
+        // getter
+        vector<Point> getVector() const {return a; }
+        int getSize() const { return Size; }
+        
         friend bool KiemTra(const Dagiac &d)
         {
             if (d.Size != 3) return false;
@@ -69,25 +75,58 @@ class Dagiac
             double ca = KhoangCach(d.a[2], d.a[0]);
             return (ab == bc || bc == ca || ca == ab);
         }
+        
+        friend ostream &operator << (const ostream &out, const Dagiac &dagiac);
+        friend istream &operator >> (istream &in, Dagiac& dagiac);
 };
+
+ostream &operator << (ostream &out, const Dagiac &dagiac) 
+{
+    for (auto point : dagiac.getVector())
+    {
+        cout << point << ", ";
+    }
+    cout << "\n-----------------------------\n";
+    return out;
+}
+
+istream &operator >> (istream &in, Dagiac& dagiac)
+{
+    for (int i = 0; i < dagiac.getSize(); ++i)
+    {
+        cin >> dagiac.a[i];
+    }
+    return in;
+}
 
 int main()
 {
     int n;
     cout << "Nhap so luong da giac: "; cin >> n;
 
-    vector<Dagiac> a(n);
+    vector<Dagiac> a;
 
     for (int i = 0; i < n; ++i)
     {
-        cout << "toa do diem thu " << i + 1 << ": ";
+        int Size; 
+        cout << "Nhap so dinh cua da giac thu " << i + 1 << ": "; cin >> Size;
+        Dagiac dagiac(Size);
+        cin >> dagiac;
+        a.push_back(dagiac);
     }
 
+    cout << "Cac da giac da nhap:\n";
+    for (int i = 0; i < n; ++i)
+    {
+        cout << "Cac diem thuoc da giac thu " << i + 1 << ": " << a[i] << endl;
+    }
+    
+    cout << "Cac da giac thoa man la tam giac:\n";
     for (int i = 0; i < n; ++i)
     {
         if (KiemTra(a[i]))
         {
-            cout << a[i];
+            cout << "Cac diem thuoc da giac thu " << i + 1 << ": " << a[i] << endl;
         }
     }        
     return 0;
