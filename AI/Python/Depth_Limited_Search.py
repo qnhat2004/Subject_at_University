3ad64aebbbfe356aa3aadd02c99ca6b9c7807cd0
Vũ Quang Nhật <126136518+qnhat2004@users.noreply.github.com>
2024-01-18 17:21:15 +0700

    tạo file depth limited search python

---
+++ AI/Python/Depth_Limited_Search.py
@@ -0,0 +1,65 @@
+''' 
+Depth Limited Search - Tìm kiếm sâu lặp
+* Hạn chế của DFS:
+- Lặp trạng thái -> giải pháp: boolean visited
+- Sâu vô hạn -> giải pháp: tìm kiếm theo chiều sâu hạn chế
+* Hạn chế của DLS:
+- Nút nằm ở độ sâu hơn không được khám phá -> không tìm được đỉnh đích
+'''
+
+from collections import defaultdict
+
+class Graph:
+    # Constructor
+    def __init__(self):
+        self.adj = defaultdict(list)
+        self.parent = defaultdict()
+    def addEdge(self, u, v):
+        self.adj[u].append(v)
+
+    def show_path(self, parent, start, goal):
+        path = []
+        while parent[goal] != None:
+            path.append(goal)
+            goal = parent[goal]
+        print(*path[::-1])
+    
+    def Depth_Limited_Search(self, start, goal, limited):
+        visited = set()
+        visited.add(start)
+        
+        self.parent[start] = None
+
+        stack = [start]
+        h = 0 # độ sâu hiện tại
+
+        while stack:
+            cur = stack.pop()
+            if cur == goal:
+                return True
+            if h == limited:
+                continue
+            for u in self.adj[cur]:
+                if u not in visited:
+                    stack.append(u)
+                    visited.add(u)
+                    h += 1
+                    self.parent[u] = cur            
+        return False
+    
+if __name__ == "__main__":
+    g = Graph()
+
+    n, m = map(int, input("Nhập số đỉnh và số cạnh: ").split())
+    for i in range(m):
+        u, v = input(f"Nhập cạnh thứ {i+1}: ").split()
+        g.addEdge(u, v)
+    
+    start, end = input("Nhập đỉnh bắt đầu và đỉnh đích: ").split()
+    limited = int(input("Nhập độ sâu giới hạn: "))
+
+    if g.Depth_Limited_Search(start, end, limited):
+        print("\nTìm thấy đường đi")
+        g.show_path(g.parent, start, end)
+    else:
+        print("\nKhông tìm thấy đường đi")
